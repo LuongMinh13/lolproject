bc9a222c5bdb0e4ba2175665da18688562840d73
LuongMinh13 <l.minh1995@yahoo.fr>
2022-01-20 16:04:49 +0100

    add https for deploy

--- src/axios/requests.js
+++ src/axios/requests.js
@@ -1,10 +1,10 @@
 export const riotapi =
-  "http://ddragon.leagueoflegends.com/cdn/12.1.1/data/en_US";
+  "https://ddragon.leagueoflegends.com/cdn/12.1.1/data/en_US";
 export const imagesplash =
-  "http://ddragon.leagueoflegends.com/cdn/img/champion/splash";
+  "https://ddragon.leagueoflegends.com/cdn/img/champion/splash";
 export const imagechampapi =
   "https://ddragon.leagueoflegends.com/cdn/img/champion/loading";
 export const imageskillapi =
-  "http://ddragon.leagueoflegends.com/cdn/12.1.1/img/spell";
+  "https://ddragon.leagueoflegends.com/cdn/12.1.1/img/spell";
 export const imagepassiveapi =
-  "http://ddragon.leagueoflegends.com/cdn/12.1.1/img/passive";
+  "https://ddragon.leagueoflegends.com/cdn/12.1.1/img/passive";
